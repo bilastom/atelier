8b15eebda6ad6b3911ea1c3e88b1e3e30b1dd82e
Tomasz Biłas <tomasz.bilas@gmail.com>
2017-11-22 19:02:58 +0100

    add migraion to drop table categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102211338) do
+ActiveRecord::Schema.define(version: 20171122180116) do
 
   create_table "authors", force: :cascade do |t|
     t.string "firstname"
@@ -45,10 +45,6 @@ ActiveRecord::Schema.define(version: 20171102211338) do
     t.index ["author_id"], name: "index_books_on_author_id"
     t.index ["category_id"], name: "index_books_on_category_id"
     t.index ["publisher_id"], name: "index_books_on_publisher_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
   end
 
   create_table "publishers", force: :cascade do |t|
